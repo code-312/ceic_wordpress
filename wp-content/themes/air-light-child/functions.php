c81ff2f4bc0e308760355da72e8277e334329693
Davey Devs <45680621+DaveyDevs@users.noreply.github.com>
2021-11-21 17:58:40 -0600

    header updates and search bar desktop draft 1

--- wp-content/themes/air-light-child/functions.php
+++ wp-content/themes/air-light-child/functions.php
@@ -16,3 +16,62 @@ register_nav_menus( array(
 	'primary' => __( 'Primary Menu', 'air-light' ),
     'secondary' => __( 'Secondary Menu', 'air-light' )
 ) );
+
+// Begin work on search bar
+function wpb_hook_javascript_footer() {
+    ?>
+        <script>
+            let searchButton = document.querySelector(".is-search-submit");
+            let searchForm = document.querySelector(".is-search-form");
+            let srText = document.querySelector(".is-screen-reader-text");
+            let textInput = document.querySelector(".is-search-input");
+
+            srText.classList.add("closed");
+            textInput.classList.add("closed");
+
+            // Updates aria label for screen reader
+            searchButton.children[0].innerHTML = "Open Search Bar";
+
+            // If focus is no longer on the search input or button, closes the search bar.
+            function checkFocus() {
+                setTimeout(function(){ 
+                    let stillInSearch = (document.activeElement === searchButton) || (document.activeElement === textInput);
+
+                    if(!stillInSearch) {
+                        searchForm.classList.remove("open");
+                        searchButton.classList.remove("opened");
+                        srText.classList.add("closed");
+                        textInput.classList.add("closed");
+
+                        searchButton.children[0].innerHTML = "Open Search Bar";
+                    }
+                 }, 100);
+            };
+
+            function openSearchForm(e) {
+
+                if (!searchButton.classList.contains("opened")) {
+                    e.preventDefault();
+
+                    searchForm.classList.add("open");
+                    searchButton.classList.add("opened");
+                    srText.classList.remove("closed");
+                    textInput.classList.remove("closed");
+
+                    searchButton.children[0].innerHTML = "Search Button";
+
+                    // Changes user focus to input
+                    textInput.focus();
+
+                    // Checks if focus has left the search input or button
+                    [searchButton, textInput].forEach(item => {
+                        item.addEventListener("blur", checkFocus)
+                    });
+                } 
+            }
+
+            searchButton.addEventListener("click", openSearchForm); 
+        </script>
+    <?php
+}
+add_action('wp_footer', 'wpb_hook_javascript_footer');
